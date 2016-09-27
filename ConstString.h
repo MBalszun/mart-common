19474fecde172721f3b1ece13aa4e410c7e261e3
Michael Balszun <michael.balszun@tum.de>
2016-09-27 17:59:36 +0200

    Put ConstString.h and StringView.h in non-experimental folder

---
+++ ConstString.h
@@ -0,0 +1,7 @@
+#ifndef MART_COMMON_CONST_STRING_H
+#define MART_COMMON_CONST_STRING_H
+#pragma once
+
+#include "./experimental/ConstString.h"
+
+#endif
---
+++ StringView.h
@@ -0,0 +1,7 @@
+#ifndef MART_LIB_COMMON_STRING_VIEW_H
+#define MART_LIB_COMMON_STRING_VIEW_H
+#pragma once
+
+#include "./experimental/StringView.h"
+
+#endif
