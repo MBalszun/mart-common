7c92da477c04c1771a688fc3e31606be9bf1b77c
Michael Balszun <michael.balszun@tum.de>
2016-09-30 13:06:20 +0200

    [Add] integral parser for mart::StringView

--- ConstString.h
+++ ConstString.h
@@ -2,6 +2,7 @@
 #define MART_COMMON_CONST_STRING_H
 #pragma once
 
+#include "StringView.h"
 #include "./experimental/ConstString.h"
 
 #endif
--- StringView.h
+++ StringView.h
@@ -4,4 +4,32 @@
 
 #include "./experimental/StringView.h"
 
+
+namespace mart {
+
+	template<class T=int>
+	T to_integral(mart::StringView str)
+	{
+		if (str.size() == 0) {
+			return T{};
+		}
+
+		typename std::make_unsigned<T>::type tmp = 0;;
+		bool neg = str[0] == '-';
+		if (neg || str[0]=='+') {
+			str = str.substr(1);
+		}
+		for (auto c : str) {
+			auto d = c - '0';
+			if ('0'<=c && c <='9') {
+				tmp *= 10;
+				tmp += d;
+			} else {
+				break;
+			}
+		}
+		return neg ? -static_cast<T>(tmp) : static_cast<T>(tmp);
+	}
+}
+
 #endif
