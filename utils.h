b87caba67fbab15a9a03fe6f5a4adac77ed4c643
Michael Balszun <michael.balszun@tum.de>
2016-11-02 12:38:36 +0100

    [Fix] non-inline declared getRandomBool function in header

--- utils.h
+++ utils.h
@@ -80,7 +80,7 @@ inline std::default_random_engine& getRandomEngine()
 
 // Shorthand to get an random integral random number within a certain range
 template <class T = int>
-T getRandomInt(T min, T max)
+inline T getRandomInt(T min, T max)
 {
 	static_assert(std::is_integral<T>::value, "Parameters must be integral type");
 	return std::uniform_int_distribution<T>(min, max)(getRandomEngine());
@@ -88,14 +88,14 @@ T getRandomInt(T min, T max)
 
 // Shorthand to get an random floating point number within a certain range
 template<class T = double>
-T getRandomFloat(T min, T max)
+inline T getRandomFloat(T min, T max)
 {
 	static_assert(std::is_floating_point<T>::value, "Parameters must be floating point type");
 	return std::uniform_real_distribution<T>(min, max)(getRandomEngine());
 }
 
 
-bool getRandomBool(double hitProb)
+inline bool getRandomBool(double hitProb)
 {
 	return std::bernoulli_distribution{hitProb}(getRandomEngine());
 }
