4a3ea131051338094ace78608c28d2b9b6f56109
Michael Balszun <michael.balszun@tum.de>
2016-11-17 12:14:18 +0100

    [MartLog] Add simple default initialization

--- MartLog.h
+++ MartLog.h
@@ -2,3 +2,15 @@
 
 #include "logging/Logger.h"
 #include "logging/Sinks.h"
+
+namespace mart {
+namespace log {
+
+inline void defaultInit(LOG_LVL logLvl = LOG_LVL::DEBUG, mart::StringView name_tag = mart::StringView("main"))
+{
+	Logger& logger = Logger::initDefaultLogger(LoggerConf_t{ mart::ConstString(name_tag) , logLvl });
+	logger.addSink(log::makeSink(log::StdOutLogConfig_t{ LOG_LVL::DEBUG }) );
+}
+
+}
+}
