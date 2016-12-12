0d511877db04f7b1abfbda32a3f778dff81431bb
Michael Balszun <michael.balszun@tum.de>
2016-12-12 18:54:27 +0100

    [Logging] Fix MART_LOG_<...>_COND macros

--- logging/MartLogFWD.h
+++ logging/MartLogFWD.h
@@ -18,15 +18,15 @@
 
 #define MART_LOG_ERROR(LOGGER,...)  		((LOGGER).log(mart::log::LOG_LVL::ERROR,__VA_ARGS__))
 #define MART_DEFLOG_ERROR(...)  		 (MART_DEFLOG.log(mart::log::LOG_LVL::ERROR,__VA_ARGS__))
-#define MART_LOG_ERROR_COND(COND,...)		do{ if (COND) { MART_LOG_ERROR(__VA_ARGS__);}while(false) }
-#define MART_DEFLOG_ERROR_COND(COND,...)	do{ if (COND) { MART_DEFLOG_ERROR(__VA_ARGS__);}while(false)  }
+#define MART_LOG_ERROR_COND(COND,...)		do{ if (COND) { MART_LOG_ERROR(__VA_ARGS__);} } while(false)
+#define MART_DEFLOG_ERROR_COND(COND,...)	do{ if (COND) { MART_DEFLOG_ERROR(__VA_ARGS__);} } while(false)
 
 #if MART_LOG_MAX_LOG_LVL >= MART_LOG_LOG_LVL_STATUS
 #define MART_LOG_STATUS(LOGGER,...) 	((LOGGER).log(mart::log::LOG_LVL::STATUS,__VA_ARGS__))
 #define MART_DEFLOG_STATUS(...) 	 (MART_DEFLOG.log(mart::log::LOG_LVL::STATUS,__VA_ARGS__))
 #define MART_LOG(...)				 (MART_DEFLOG.log(mart::log::LOG_LVL::STATUS,__VA_ARGS__))
-#define MART_LOG_STATUS_COND(COND,...)		do{ if (COND) { MART_LOG_STATUS(__VA_ARGS__); }while(false) }
-#define MART_DEFLOG_STATUS_COND(COND,...)	do{ if (COND) { MART_DEFLOG_STATUS(__VA_ARGS__); }while(false) }
+#define MART_LOG_STATUS_COND(COND,...)		do{ if (COND) { MART_LOG_STATUS(__VA_ARGS__); } } while(false)
+#define MART_DEFLOG_STATUS_COND(COND,...)	do{ if (COND) { MART_DEFLOG_STATUS(__VA_ARGS__); } } while(false)
 #else
 #define MART_LOG_STATUS(LOGGER,...) (void)0
 #define MART_DEFLOG_STATUS(...) 	(void)0
@@ -38,8 +38,8 @@
 #if MART_LOG_MAX_LOG_LVL >= MART_LOG_LOG_LVL_DEBUG
 #define MART_LOG_DEBUG(LOGGER,...)  	((LOGGER).log(mart::log::LOG_LVL::DEBUG,__VA_ARGS__))
 #define MART_DEFLOG_DEBUG(...)  	 (MART_DEFLOG.log(mart::log::LOG_LVL::DEBUG,__VA_ARGS__))
-#define MART_LOG_DEBUG_COND(COND,...)		do{ if (COND) { MART_LOG_DEBUG(__VA_ARGS__);}while(false)  }
-#define MART_DEFLOG_DEBUG_COND(COND,...)	do{ if (COND) { MART_DEFLOG_DEBUG(__VA_ARGS__);}while(false)  }
+#define MART_LOG_DEBUG_COND(COND,...)		do{ if (COND) { MART_LOG_DEBUG(__VA_ARGS__);} } while(false)
+#define MART_DEFLOG_DEBUG_COND(COND,...)	do{ if (COND) { MART_DEFLOG_DEBUG(__VA_ARGS__);} } while(false)
 #else
 #define MART_LOG_DEBUG(LOGGER,...) 			(void)0
 #define MART_DEFLOG_DEBUG(...) 				(void)0
@@ -50,8 +50,8 @@
 #if MART_LOG_MAX_LOG_LVL >= MART_LOG_LOG_LVL_TRACE
 #define MART_LOG_TRACE(LOGGER,...)  		((LOGGER).log(mart::log::LOG_LVL::TRACE,__VA_ARGS__))
 #define MART_DEFLOG_TRACE(...)  		 (MART_DEFLOG.log(mart::log::LOG_LVL::TRACE,__VA_ARGS__))
-#define MART_LOG_TRACE_COND(COND,...)		do{ if (COND) { MART_LOG_TRACE(__VA_ARGS__); } while(false) }
-#define MART_DEFLOG_TRACE_COND(COND,...)	do{ if (COND) { MART_DEFLOG_TRACE(__VA_ARGS__); } while(false) }
+#define MART_LOG_TRACE_COND(COND,...)		do{ if (COND) { MART_LOG_TRACE(__VA_ARGS__); } } while(false)
+#define MART_DEFLOG_TRACE_COND(COND,...)	do{ if (COND) { MART_DEFLOG_TRACE(__VA_ARGS__); } }while(false)
 #else
 #define MART_LOG_TRACE(LOGGER,...) 			(void)0
 #define MART_DEFLOG_TRACE(...) 				(void)0
