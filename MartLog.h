191f5ffea62bbb80258d3a64bbf4d5e2d4898ead
Michael Balszun <michael.balszun@tum.de>
2017-03-22 11:04:10 +0100

    Git add log initialization adding a file name

--- MartLog.h
+++ MartLog.h
@@ -12,5 +12,11 @@ inline void defaultInit(LOG_LVL logLvl = LOG_LVL::DEBUG, mart::StringView name_t
 	logger.addSink(log::makeSink(log::StdOutLogConfig_t{ LOG_LVL::DEBUG }) );
 }
 
+inline void defaultInit(mart::StringView file_tag, LOG_LVL logLvl = LOG_LVL::DEBUG, mart::StringView name_tag = mart::StringView("main"))
+{
+	Logger& logger = Logger::initDefaultLogger(LoggerConf_t{ mart::ConstString(name_tag) , logLvl });
+	logger.addSink(log::makeSink(log::FileLogConfig_t{ mart::ConstString{file_tag},LOG_LVL::TRACE }));
+}
+
 }
 }
