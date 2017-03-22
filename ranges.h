0abf7b7d7b114220df243de5dad6ae81ae987cf0
Michael Balszun <michael.balszun@tum.de>
2017-03-22 11:00:51 +0100

    [Add] experimental floating point range iterator that can be used similar to matlabs [<min>:<step>:<max>] command

--- ranges.h
+++ ranges.h
@@ -56,8 +56,8 @@ namespace mart {
 		using reference = const T&;
 		using iterator_category = std::random_access_iterator_tag;
 
-		IIterator(T value = T()) :
-			i{ value }
+		IIterator(T value = T())
+			: i{ value }
 		{};
 
 		reference operator*() const { return i; }
@@ -161,8 +161,129 @@ _impl_irange::irange_t<typename C::size_type> irange(const C& container) {
 	return _impl_irange::irange_t<typename C::size_type>{0, container.size()};
 }
 
+template<class T, size_t N >
+_impl_irange::irange_t<size_t> irange(const T(&other)[N]) {
+	return _impl_irange::irange_t<size_t>{0, N};
+}
+
+
+
+namespace experimental {
+namespace _impl_frange {
+	template <class T>
+	struct frange_t;
+}
+
+template<class T>
+class FIterator {
+	static_assert(std::is_floating_point<T>::value, "FIterator can only be used for floating point values");
+public:
+	using difference_type = std::ptrdiff_t;
+	using value_type = T;
+	using pointer = T*;
+	using reference = const T&;
+	using iterator_category = std::random_access_iterator_tag;
+
+	FIterator(T value = T(), T step = T(1.0))
+		: i{ 0 }
+		, offset{ value }
+		, step{step}
+	{}
+
+	value_type operator*() const { return i*step + offset; }
+	//pointer operator->() const { return &i; }
+
+	FIterator& operator++() { ++i; return *this; }
+	FIterator operator++(int) { return FIterator{ i++ }; }
+
+	FIterator& operator--() { --i; return *this; }
+	FIterator operator--(int) { return FIterator{ i-- }; }
+
+	FIterator& operator+=(difference_type diff) { i += diff; return *this; }
+	FIterator& operator-=(difference_type diff) { i -= diff; return *this; }
+
+	value_type operator[](difference_type diff) const { return i + diff; }
+
+	friend bool operator!=(FIterator l, FIterator r) { assert(l.offset == r.offset && l.step == r.step); return l.i != r.i; }
+	friend bool operator==(FIterator l, FIterator r) { assert(l.offset == r.offset && l.step == r.step); return l.i == r.i; }
+	friend bool operator<(FIterator l, FIterator r) { assert(l.offset == r.offset && l.step == r.step); return l.i < r.i; }
+	friend bool operator<=(FIterator l, FIterator r) { assert(l.offset == r.offset && l.step == r.step); return l.i <= r.i; }
+	friend bool operator>(FIterator l, FIterator r) { assert(l.offset == r.offset && l.step == r.step); return l.i > r.i; }
+	friend bool operator>=(FIterator l, FIterator r) { assert(l.offset == r.offset && l.step == r.step); return l.i >= r.i; }
+
+	friend FIterator operator+(FIterator it, difference_type n) {
+		it.i += n;
+		return it;
+	}
+	friend FIterator operator+(difference_type n, FIterator it) { return it + n; }
+
+	friend difference_type operator-(FIterator l, FIterator r) {
+		assert(l.offset == r.offset && l.step == r.step);
+		return l.i - r.i;
+	}
+	friend FIterator operator-(FIterator it, difference_type n) {
+		it.i -= n;
+		return it;
+	}
+	friend FIterator operator-(difference_type n, FIterator it) { return it - n; }
+
+private:
+	template<class>
+	friend struct _impl_frange::frange_t;
+
+	std::ptrdiff_t i;
+	T offset;
+	T step;
+};
+
+namespace _impl_frange {
+template <class T>
+struct frange_t {
+	static_assert(std::is_floating_point<T>::value, "frange can only be used for floating point values");
+	using iterator = FIterator<T>;
+
+	iterator begin() const {
+		return iterator{ _start_v,_step };
+
+	};
+	iterator end() const {
+		iterator ret{ _start_v,_step };
+		ret.i = static_cast<std::ptrdiff_t>((_end_v - _start_v) / _step) + 1;
+		return ret;
+	};
+
+	frange_t(T start, T end, T step = T(1.0))
+		:_start_v{start}
+		, _end_v{end}
+		, _step{step}
+	{
+	}
+
+	T _start_v;
+	T _end_v;
+	T _step;
+	frange_t step(T value) const {
+		frange_t n(*this);
+		n._step = value;
+		return n;
+	}
+};
+} //_impl_frange
+
+template<class T>
+_impl_frange::frange_t<T> frange(_impl_irange::non_deduced_t<T> start, T end) {
+	return _impl_frange::frange_t<T>{ start,end};
+}
+
+
+
+} //experimental
+
+
 
 }
+
+
 
 namespace std {
 	template<class IT>
