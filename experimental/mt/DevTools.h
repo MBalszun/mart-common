377f660f70013a2e40c71f642ba2de96e88ad4b6
Michael Balszun <michael.balszun@tum.de>
2017-04-11 13:15:11 +0200

    [Fix] and rename dev::TimedExec() to dev::execTimed()

--- experimental/mt/DevTools.h
+++ experimental/mt/DevTools.h
@@ -7,12 +7,12 @@ namespace experimental {
 namespace dev {
 
 template<class F>
-std::chrono::nanoseconds TimedExec(F&& f)
+std::chrono::nanoseconds execTimed(F&& f)
 {
 	using tclock = std::chrono::steady_clock;
 	auto start = tclock::now();
 	f();
-	auto end = stclock::now();
+	auto end = tclock::now();
 	return end - start;
 }
 
