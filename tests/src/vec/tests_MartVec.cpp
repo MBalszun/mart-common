803c7857a5bb85286d5bb84769d107449e00c4fc
Michael Balszun <michael.balszun@tum.de>
2018-03-26 17:59:46 +0200

    [Tests/Vec] Add tests for elementwise comparison of mart::Vec

--- tests/src/vec/tests_MartVec.cpp
+++ tests/src/vec/tests_MartVec.cpp
@@ -1,26 +1,155 @@
 #include <mart-common/MartVec.h>
 
 #include <catch2/catch.hpp>
+#include <iostream>
 #include <type_traits>
 
-
-namespace {
-	struct Wrapper {
-		int v;
-		friend Wrapper operator*(Wrapper l, int r) { return { l.v*r }; }
-		friend Wrapper operator*(int l, Wrapper r) { return { l*r.v }; }
-		friend Wrapper operator*(Wrapper l, Wrapper r) { return { l.v*r.v }; }
-		friend bool operator==(Wrapper l, Wrapper r) { return l.v==r.v; }
-	};
+template<class T, int N>
+std::ostream& operator<<( std::ostream& out, mart::Vec<T, N> vec )
+{
+	out << "[";
+	for( int i = 0; i < N; ++i ) {
+		out << vec[i] << ", ";
+	}
+	out << "]";
+	return out;
 }
 
-TEST_CASE("Some_random_vector_math_code", "[vec]")
+namespace {
+struct Wrapper {
+	int			   v;
+	friend Wrapper operator*( Wrapper l, int r ) { return {l.v * r}; }
+	friend Wrapper operator*( int l, Wrapper r ) { return {l * r.v}; }
+	friend Wrapper operator*( Wrapper l, Wrapper r ) { return {l.v * r.v}; }
+	friend bool	operator==( Wrapper l, Wrapper r ) { return l.v == r.v; }
+};
+}
+
+TEST_CASE( "MartVec_some_random_vector_math_code", "[vec]" )
 {
 
-	const mart::Vec3D<Wrapper> expected{ {1}, {2}, {-2} };
+	const mart::Vec3D<Wrapper> expected{{1}, {2}, {-2}};
 
-	const mart::Vec3D<Wrapper> base{ {1}, {1}, {2} };
-	const auto 	r = base * mart::Vec3D<int>{1, 2, -1};
-	static_assert(std::is_same_v<decltype(expected), decltype(r)>);
-	CHECK(expected == r);
+	const mart::Vec3D<Wrapper> base{{1}, {1}, {2}};
+	const auto				   r = base * mart::Vec3D<int>{1, 2, -1};
+	static_assert( std::is_same_v<decltype( expected ), decltype( r )> );
+	CHECK( expected == r );
+}
+
+namespace {
+
+template<class T, int N>
+mart::Vec<T, N> generateOnes()
+{
+	mart::Vec<T, N> v{};
+	for( int i = 0; i < N; ++i ) {
+		v[i] = 1;
+	}
+	return v;
+}
+
+template<class T, int N>
+mart::Vec<T, N> generate1()
+{
+	mart::Vec<T, N> vec{};
+	T				v{-N / 2};
+	for( int i = 0; i < N; ++i ) {
+		vec[i] = v;
+		v	  = v + T{1};
+	}
+	return vec;
+}
+
+template<class T, int N>
+mart::Vec<T, N> generate2()
+{
+	mart::Vec<T, N> vec{};
+	T				v{0};
+	for( int i = 0; i < N; ++i ) {
+		vec[i] = v;
+		v	  = v + T{1};
+	}
+	return vec;
+}
+
+template<class T, int N>
+mart::Vec<T, N> generate3()
+{
+	mart::Vec<T, N> vec{};
+	T				v{0};
+	for( int i = 0; i < N; ++i ) {
+		vec[i] = v;
+		v	  = v * 2;
+	}
+	return vec;
+}
+
+template<class T, int N>
+void check_addition_subtraction()
+{
+	auto v1		= generate1<T, N>();
+	auto v2		= generate2<T, N>();
+	auto v3		= v1 + v2;
+
+	CHECK( v1 == v3 - v2 );
+}
+
+template<class T, int N>
+void check_multiplication_division()
+{
+	auto v1		= generate1<T, N>();
+	auto v2		= generate1<T, N>() * generate1<T, N>() + generateOnes<T, N>();
+	auto v3		= v1 * v2;
+
+	CHECK( v1 == v3 / v2 );
+}
+}
+
+TEST_CASE( "MartVec_same_type_2d_operations", "[vec]" )
+{
+	check_addition_subtraction<int, 1>();
+	check_addition_subtraction<int, 2>();
+	check_addition_subtraction<int, 3>();
+	check_addition_subtraction<int, 4>();
+	check_addition_subtraction<int, 5>();
+	check_addition_subtraction<int, 6>();
+	check_addition_subtraction<int, 7>();
+}
+
+TEST_CASE( "MartVec_same_type_3d_operations", "[vec]" )
+{
+	check_multiplication_division<int, 1>();
+	check_multiplication_division<int, 2>();
+	check_multiplication_division<int, 3>();
+	check_multiplication_division<int, 4>();
+	check_multiplication_division<int, 5>();
+	check_multiplication_division<int, 6>();
+	check_multiplication_division<int, 7>();
+}
+
+
+TEST_CASE("MartVec_element_comparison", "[vec]")
+{
+	using Vec_t = mart::Vec<int, 5>;
+	Vec_t vec1{ 1, 2, 3, 4, 5 };
+	Vec_t vec2{ 2, 2, 2, 2, 2 };
+
+	using BVec_t = mart::Vec<bool, 5>;
+
+	CHECK(mart::elementLess(vec1, vec2) == BVec_t{ true, false, false, false, false });
+	CHECK(mart::elementLessEqual(vec1, vec2) == BVec_t{ true, true, false, false, false });
+	CHECK(mart::elementGreater(vec1, vec2) == BVec_t{ false, false, true, true, true });
+	CHECK(mart::elementGreaterEqual(vec1, vec2) == BVec_t{ false, true, true, true, true });
+	CHECK(mart::elementNE(vec1, vec2) == BVec_t{ true, false, true, true, true });
+	CHECK(mart::elementEquals(vec1, vec2) == BVec_t{ false, true, false, false, false });
+}
+
+TEST_CASE("MartVec_element_logic", "[vec]")
+{
+	using BVec_t = mart::Vec<bool, 5>;
+	BVec_t vec1{ true, false, true, false, true };
+	BVec_t vec2{ true, true, false, false, false };
+
+	CHECK(mart::elementAnd(vec1, vec2) == BVec_t{ true, false, false, false, false });
+	CHECK(mart::elementOr(vec1, vec2) == BVec_t{ true, true, true, false, true });
 }
