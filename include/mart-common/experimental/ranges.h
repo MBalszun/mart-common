2d0accb5c9016e446c81bf837eab28551b1717e4
Michael Balszun <michael.balszun@tum.de>
2018-04-26 17:02:08 +0200

    [WS] Run clang-format over include/mart-common/experimental/ranges.h

--- include/mart-common/experimental/ranges.h
+++ include/mart-common/experimental/ranges.h
@@ -14,8 +14,8 @@
  *
  */
 
-#include "../ranges.h"
 #include "../enum/EnumHelpers.h"
+#include "../ranges.h"
 
 namespace mart {
 namespace experimental {
@@ -24,42 +24,44 @@ template<class It>
 struct range {
 	It _begin;
 	It _end;
+
 	constexpr It begin() const { return _begin; }
 	constexpr It end() const { return _end; }
 };
 
 template<class R>
-auto view_reversed(R&& r) -> range<std::reverse_iterator<decltype(r.begin())>> {
-	using Rit = std::reverse_iterator<decltype(r.begin())>;
-	return{ Rit{ r.end() },Rit{ r.begin() } };
+auto view_reversed( R&& r ) -> range<std::reverse_iterator<decltype( r.begin() )>>
+{
+	using Rit = std::reverse_iterator<decltype( r.begin() )>;
+	return {Rit{r.end()}, Rit{r.begin()}};
 }
 
-template <class Enum, class Ut = mart::underlying_type_t<Enum>>
+template<class Enum, class Ut = mart::underlying_type_t<Enum>>
 class DefaultEnumRange {
 public:
-	constexpr Enum operator[](int i) const { return static_cast<Enum>(i); }
+	constexpr Enum operator[]( int i ) const { return static_cast<Enum>( i ); }
 
 	struct Iterator {
-		constexpr Iterator(Enum e)
-			: idx{ toUType(e) }
+		constexpr Iterator( Enum e )
+			: idx{toUType( e )}
 		{
 		}
-		constexpr Enum		   operator*() const { return static_cast<Enum>(idx); }
+		constexpr Enum		   operator*() const { return static_cast<Enum>( idx ); }
 		/*constexpr*/ Iterator operator++() { return ++idx; }
-		Iterator			   operator++(int)
+		Iterator			   operator++( int )
 		{
 			auto t = *this;
 			idx++;
 			return t;
 		}
-		friend bool operator!=(Iterator l, Iterator r) { return l.idx != r.idx; }
-		friend bool operator!=(Iterator l, Iterator r) { return l.idx == r.idx; }
+		friend bool operator!=( Iterator l, Iterator r ) { return l.idx != r.idx; }
+		friend bool operator!=( Iterator l, Iterator r ) { return l.idx == r.idx; }
 
 	private:
 		Ut idx;
 	};
-	constexpr Iterator begin() const { return Iterator{ first }; }
-	constexpr Iterator end() const { return Iterator{ static_cast<Enum>(static_cast<Ut>(last) + Ut{ 1 }) }; }
+	constexpr Iterator begin() const { return Iterator{first}; }
+	constexpr Iterator end() const { return Iterator{static_cast<Enum>( static_cast<Ut>( last ) + Ut{1} )}; }
 	const Enum		   first;
 	const Enum		   last;
 };
