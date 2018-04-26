a16a15d47cdee9b5394e8ec0485d283f578f31aa
Michael Balszun <michael.balszun@tum.de>
2018-04-26 17:08:54 +0200

    [Tests/algo] Run stub tests for parallel algorithms if unsupported

--- tests/src/algorithm/tests_sort.cpp
+++ tests/src/algorithm/tests_sort.cpp
@@ -5,6 +5,19 @@
 #include <functional>
 #include <vector>
 
+#ifndef MART_COMMON_TEST_PARALLEL_ALGORITHMS
+#define MART_COMMON_TEST_PARALLEL_ALGORITHMS 0
+#endif
+
+#ifdef _MSC_VER
+#undef MART_COMMON_TEST_PARALLEL_ALGORITHMS
+#define MART_COMMON_TEST_PARALLEL_ALGORITHMS 1
+#endif
+
+#if MART_COMMON_TEST_PARALLEL_ALGORITHMS
+#include <execution>
+#endif
+
 TEST_CASE( "algo_sort_range_is_sorted_after_sorting", "[algorithm][sort]" )
 {
 	std::vector<int> v{94, -1, 5, 3, 66, 3};
@@ -21,24 +34,29 @@ TEST_CASE( "algo_sort_range_is_reverse_sorted_after_reverse_sorting", "[algorith
 	CHECK( mart::is_sorted( v, std::greater<>{} ) );
 }
 
-#ifdef _MSC_VER
-
-#include <execution>
-
 TEST_CASE( "algo_sort_range_is_sorted_after_parallel_sorting", "[algorithm][sort][mt]" )
 {
+#if MART_COMMON_TEST_PARALLEL_ALGORITHMS
+
 	std::vector<int> v{94, -1, 5, 3, 66, 3};
 
 	mart::sort( std::execution::par, v );
 	CHECK( mart::is_sorted( v ) );
+
+#else
+	CHECK( true );
+#endif
 }
 
 TEST_CASE( "algo_sort_range_is_reverse_sorted_after_parallel_reverse_sorting", "[algorithm][sort][mt]" )
 {
+#if MART_COMMON_TEST_PARALLEL_ALGORITHMS
+
 	std::vector<int> v{94, -1, 5, 3, 66, 3};
-
-	mart::sort(std::execution::par, v, std::greater<>{} );
+	mart::sort( std::execution::par, v, std::greater<>{} );
 	CHECK( mart::is_sorted( v, std::greater<>{} ) );
-}
 
+#else
+	CHECK( true );
 #endif
+}
