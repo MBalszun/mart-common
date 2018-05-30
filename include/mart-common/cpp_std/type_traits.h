fa80cf4129b6752937838b46c6e3b97a07d4f33a
Michael Balszun <michael.balszun@tum.de>
2018-05-30 15:20:17 +0200

    [cpp_std] Add implementation of std::experimental::is_detected

--- include/mart-common/cpp_std/type_traits.h
+++ include/mart-common/cpp_std/type_traits.h
@@ -56,5 +56,41 @@ using std::disjunction_v;
 using std::aligned_union;
 using std::aligned_union_t;
 
-}
+struct nonesuch {
+	nonesuch()					= delete;
+	~nonesuch()					= delete;
+	nonesuch( nonesuch const& ) = delete;
+	void operator=( nonesuch const& ) = delete;
+};
+
+/*######### Detection Idiom #########*/
+
+// http://en.cppreference.com/w/cpp/experimental/is_detected
+
+namespace detail_detection {
+template<class Default, class AlwaysVoid, template<class...> class Op, class... Args>
+struct detector {
+	using value_t = std::false_type;
+	using type	= Default;
+};
+
+template<class Default, template<class...> class Op, class... Args>
+struct detector<Default, std::void_t<Op<Args...>>, Op, Args...> {
+	// Note that std::void_t is a C++17 feature
+	using value_t = std::true_type;
+	using type	= Op<Args...>;
+};
+
+} // namespace detail_detection
+
+template<template<class...> class Op, class... Args>
+using is_detected = typename detail_detection::detector<nonesuch, void, Op, Args...>::value_t;
+
+template<template<class...> class Op, class... Args>
+using detected_t = typename detail_detection::detector<nonesuch, void, Op, Args...>::type;
+
+template<class Default, template<class...> class Op, class... Args>
+using detected_or = detail_detection::detector<Default, void, Op, Args...>;
+
+} // namespace mart
 #endif
