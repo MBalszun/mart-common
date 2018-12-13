b39cf4f6817015ec16f326476f830c85dde7d319
Michael Balszun <michael.balszun@tum.de>
2018-12-13 16:55:32 +0100

    [Tests] Add rudimentary tests for print wrappers

--- tests/src/tests_PrintWrappers.cpp
+++ tests/src/tests_PrintWrappers.cpp
@@ -1,1 +1,42 @@
 #include <mart-common/PrintWrappers.h>
+
+#include <catch2/catch.hpp>
+
+#include <type_traits>
+
+#include <array>
+#include <sstream>
+
+TEST_CASE( "Padded", "[PrintWrappers]" )
+{
+	std::stringstream ss;
+
+	ss << mart::padded( "abcd", 8, mart::Pad::Left );
+	CHECK( ss.str() == "    abcd" );
+	ss.str( "" );
+
+	ss << mart::padded( "abcd", 8, mart::Pad::Middle );
+	CHECK( ss.str() == "  abcd  " );
+	ss.str( "" );
+
+	ss << mart::padded( "abcd", 8, mart::Pad::Right );
+	CHECK( ss.str() == "abcd    " );
+	ss.str( "" );
+
+	for( int i = 0; i < 8; ++i ) {
+		ss << mart::padded( "abcd", i, mart::Pad::Left );
+		CHECK( ss.str().size() == std::max( 4, i ) );
+		ss.str( "" );
+	}
+}
+
+TEST_CASE( "Memory view", "[PrintWrappers]" )
+{
+	std::array<int, 40> data{};
+	std::iota( data.begin(), data.end(), 0 );
+
+	std::stringstream ss;
+	ss << mart::sformat( mart::view_bytes( data ) );
+
+	CHECK( true );
+}
