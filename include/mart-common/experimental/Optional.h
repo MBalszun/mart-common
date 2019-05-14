13a389d2c1750d6e514e4303525e1c587f19b0e2
Michael Balszun <michael.balszun@tum.de>
2019-05-14 17:42:42 +0200

    [Optional] constexpr, formatting and add compatibility functions

--- include/mart-common/experimental/Optional.h
+++ include/mart-common/experimental/Optional.h
@@ -14,57 +14,69 @@
  *
  */
 
-#include <stdexcept>
 #include <cstdint>
+#include <stdexcept>
 
 namespace mart {
-		template <class T>
-		class Optional {
-			enum class OPT_FLAG : std::uint8_t {
-				Invalid = 0,
-				Valid
-			};
-			T data;
-			OPT_FLAG flag = OPT_FLAG::Invalid;
+template<class T>
+class Optional {
+	enum class OPT_FLAG : std::uint8_t { Invalid = 0, Valid };
+	T        data;
+	OPT_FLAG flag = OPT_FLAG::Invalid;
 
-			void throwIfInvalid() const {
-				if (flag != OPT_FLAG::Valid) {
-					throw std::invalid_argument("Access to invalid data of Optional class");
-				}
-			}
+	constexpr void throwIfInvalid() const
+	{
+		if( flag != OPT_FLAG::Valid ) { throw std::invalid_argument( "Access to invalid data of Optional class" ); }
+	}
 
-		public:
-			Optional() = default;
-			Optional(const T& data) : data(data)			, flag(OPT_FLAG::Valid) {};
-			Optional(T&& data)		: data(std::move(data))	, flag(OPT_FLAG::Valid) {};
+public:
+	constexpr Optional() = default;
+	constexpr Optional( const T& data )
+		: data( data )
+		, flag( OPT_FLAG::Valid ) {};
+	constexpr Optional( T&& data )
+		: data( std::move( data ) )
+		, flag( OPT_FLAG::Valid ) {};
 
-			Optional(const Optional<T>& other) = default;
-			Optional(Optional<T>&& other) = default;
-			Optional& operator=(const Optional<T>& other) = default;
-			Optional& operator=(Optional<T>&& other) = default;
+	constexpr Optional( const Optional<T>& other ) = default;
+	constexpr Optional( Optional<T>&& other )      = default;
+	constexpr Optional& operator=( const Optional<T>& other ) = default;
+	constexpr Optional& operator=( Optional<T>&& other ) = default;
 
-			const T& getData() const {
-				throwIfInvalid();
-				return data;
-			}
+	constexpr const T& getData() const
+	{
+		throwIfInvalid();
+		return data;
+	}
 
-			void setData(const T& data) {
-				this->data = data;
-				this->flag = OPT_FLAG::Valid;
-			}
+	constexpr const T& value() const
+	{
+		throwIfInvalid();
+		return data;
+	}
 
-			const T& 	operator*() const	{ return data; }
-			T&			operator*()			{ return data; }
-			const T*	operator->() const	{ return &data; }
-			T*			operator->()		{ return &data; }
+	constexpr T& value()
+	{
+		throwIfInvalid();
+		return data;
+	}
 
-			explicit operator bool()	const { return isValid(); }
-			OPT_FLAG getState()			const { return flag; }
-			bool isValid()				const { return flag == OPT_FLAG::Valid; }
+	constexpr void setData( const T& data )
+	{
+		this->data = data;
+		this->flag = OPT_FLAG::Valid;
+	}
 
-		};
-}
+	constexpr const T& operator*() const { return data; }
+	constexpr       T& operator*() { return data; }
+	constexpr const T* operator->() const { return &data; }
+	constexpr       T* operator->() { return &data; }
 
-
+	constexpr explicit operator bool() const { return isValid(); }
+	constexpr OPT_FLAG getState() const { return flag; }
+	constexpr bool     isValid() const { return flag == OPT_FLAG::Valid; }
+	constexpr bool     has_value() const { return flag == OPT_FLAG::Valid; }
+};
+} // namespace mart
 
 #endif /* LIBS_C2_API_LIBS_COMMON_OPTIONAL_H_ */
