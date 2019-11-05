34c01fce99c069a75ee07bebc0d932cc6aa40c3c
Michael Balszun <michael.balszun@tum.de>
2019-11-05 15:33:03 +0100

    [netlib] Add missing headerguard and file comment

--- include/mart-netlib/network_exceptions.hpp
+++ include/mart-netlib/network_exceptions.hpp
@@ -1,3 +1,19 @@
+#ifndef LIB_MART_COMMON_GUARD_NW_NETWORK_EXCEPTIONS_H
+#define LIB_MART_COMMON_GUARD_NW_NETWORK_EXCEPTIONS_H
+/**
+ * network exceptions.hpp (mart-netlib)
+ *
+ * Copyright (C) 2019: Michael Balszun <michael.balszun@mytum.de>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See either the LICENSE file in the library's root
+ * directory or http://opensource.org/licenses/MIT for details.
+ *
+ * @author: Michael Balszun <michael.balszun@tum.de>
+ * @brief:  Provides exception definitions for usage in network types
+ *
+ */
+
 #include <exception>
 #include <mart-common/ConstString.h>
 
@@ -28,3 +44,4 @@ struct invalid_address_string final : generic_nw_error_base {
 	using generic_nw_error_base::generic_nw_error_base;
 };
 } // namespace mart::nw
+#endif
