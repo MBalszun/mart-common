edc88c6f9ce65c8d7f435b4ae329ea2d880bf427
Michael Balszun <michael.balszun@tum.de>
2019-12-12 19:10:52 +0100

    [netlib] Add more examples

---
+++ examples/nw/send_status_vals.cpp
@@ -0,0 +1,89 @@
+#include <mart-common/MartTime.h>
+#include <mart-common/utils.h>
+#include <mart-netlib/udp.hpp>
+
+#include <Windows.h>
+
+#include <iostream>
+
+#include <cstddef>
+
+std::byte b;
+
+using namespace std::chrono_literals;
+
+namespace udp = mart::nw::ip::udp;
+
+const auto destination = udp::try_parse_v4_endpoint( "127.0.0.1:3435" ).value();
+
+const char vt_cmd_cursor_reset[] = {0x1B, '[', '0', ';', '0', 'H'};
+const char vt_cmd_clear_line[]   = {0x1B, '[', '1', 'M'};
+const char vt_cmd_hide_cursor[]  = {0x1B, '[', '?', '2', '5', 'l'};
+
+template<std::size_t N>
+void insert_vt_cmd( const char ( &cmd )[N] )
+{
+	std::cout.write( cmd, sizeof( cmd ) );
+}
+
+void clear_top_console_line()
+{
+	insert_vt_cmd( vt_cmd_cursor_reset );
+	insert_vt_cmd( vt_cmd_clear_line );
+}
+
+bool is_key_pressed( char key )
+{
+	return GetKeyState( key ) & 0x8000;
+}
+
+int main()
+{
+	mart::ExecuteOnExit_t cleanup( [] {
+
+		udp::Socket {}.sendto( mart::view_bytes( "EXIT" ).subview(0,4), destination );
+	} );
+	std::atomic_int       heart_rate = 0;
+	std::atomic_int       blood_ox   = 0;
+	bool                  changed    = true;
+
+	insert_vt_cmd( vt_cmd_hide_cursor );
+
+	for( mart::PeriodicScheduler sched( 50ms );; sched.sleep() ) {
+
+		if( is_key_pressed( 'W' ) ) {
+			heart_rate++;
+			changed = true;
+		}
+		if( is_key_pressed( 'S' ) ) {
+			heart_rate--;
+			changed = true;
+		}
+		if( is_key_pressed( 'E' ) ) {
+			blood_ox++;
+			changed = true;
+		}
+		if( is_key_pressed( 'D' ) ) {
+			blood_ox--;
+			changed = true;
+		}
+
+		if( is_key_pressed( 'K' ) ) {
+			// udp::Socket {}.sendto( mart::view_bytes( "EXIT" ), destination );
+			return 0;
+		}
+
+		blood_ox   = mart::clamp( blood_ox.load(), 0, 100 );
+		heart_rate = mart::clamp( heart_rate.load(), 0, 200 );
+
+		if( changed ) {
+			auto msg = std::to_string( heart_rate ) + ":" + std::to_string( blood_ox );
+			/*std::cout.write( vt_cmd_cursor_reset, sizeof( vt_cmd_cursor_reset ) );
+			std::cout.write( vt_cmd_clear_line, sizeof( vt_cmd_clear_line ) );*/
+			clear_top_console_line();
+			std::cout << msg;
+			udp::Socket {}.sendto( mart::view_elements( msg ).asBytes(), destination );
+		}
+		changed = false;
+	}
+}
---
+++ examples/nw/write_udp_msg_to_file.cpp
@@ -0,0 +1,69 @@
+#include <mart-common/MartTime.h>
+#include <mart-common/algorithm.h>
+#include <mart-netlib/udp.hpp>
+
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <string_view>
+#include <vector>
+
+#include <im_str/im_str.hpp>
+
+namespace udp = mart::nw::ip::udp;
+using namespace std::chrono_literals;
+
+std::vector<std::string_view> make_arglist( int argc, char** argv )
+{
+	std::vector<std::string_view> ret;
+	for( int i = 0; i < argc; ++i ) {
+		ret.emplace_back( argv[i] );
+	}
+	return ret;
+}
+
+std::optional<udp::endpoint> get_local_address( const std::vector<std::string_view>& args )
+{
+	return udp::try_parse_v4_endpoint( "127.0.0.1:3435" );
+}
+
+std::optional<std::string> get_filename( const std::vector<std::string_view>& args )
+{
+	return std::string {"Testfile"};
+}
+
+std::string_view to_stringview( mart::ConstMemoryView data )
+{
+	return std::string_view {data.asConstCharPtr(), data.size()};
+}
+
+int main( int argc, char** argv )
+{
+	const auto arglist = make_arglist( argc, argv );
+
+	const udp::endpoint local_ep  = get_local_address( arglist ).value();
+	const auto          file_name = get_filename( arglist ).value();
+
+	udp::Socket sock;
+	sock.set_rx_timeout( 1000ms );
+	sock.bind( local_ep );
+
+	std::cout << "Listening on " << local_ep.toStringEx() << " and writing to" << file_name << std::endl;
+
+	std::string buffer( 1000, '\0' );
+
+	while( true ) {
+		auto res = sock.try_recv( mart::view_elements_mutable( buffer ).asBytes() );
+
+		if( res.isValid() ) {
+			auto msg = to_stringview( res );
+			if( msg == "EXIT" ) { return 0; }
+			std::ofstream file( file_name, std::ios_base::out | std::ios_base::trunc );
+			file.seekp( 0 );
+			file << msg << "\n";
+			std::cout << "\"" << msg << "\"" << std::endl;
+		}
+	}
+	std::cout << "Server shutting down\n" << std::flush;
+}
