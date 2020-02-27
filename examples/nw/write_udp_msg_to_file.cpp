4d1844dac6ae08a9bace7b725766c6bc6d1a78be
Michael Balszun <michael.balszun@tum.de>
2020-02-27 14:41:17 +0100

    [netlib] Remove domain socket files before socket creation and update example

--- examples/nw/write_udp_msg_to_file.cpp
+++ examples/nw/write_udp_msg_to_file.cpp
@@ -16,20 +16,24 @@ using namespace std::chrono_literals;
 
 std::vector<std::string_view> make_arglist( int argc, char** argv )
 {
-	std::vector<std::string_view> ret;
+	if( argc <= 0 ) { return {}; }
+
+	std::vector<std::string_view> ret( argc );
 	for( int i = 0; i < argc; ++i ) {
-		ret.emplace_back( argv[i] );
+		ret[i] = std::string_view( argv[i] );
 	}
 	return ret;
 }
 
-std::optional<udp::endpoint> get_local_address( const std::vector<std::string_view>& args )
+std::optional<udp::endpoint> get_local_address( mart::ArrayView<const std::string_view> )
 {
+	// TODO actually esxtract from args
 	return udp::try_parse_v4_endpoint( "127.0.0.1:3435" );
 }
 
-std::optional<std::string> get_filename( const std::vector<std::string_view>& args )
+std::optional<std::string> get_filename( mart::ArrayView<const std::string_view> )
 {
+	// TODO actually esxtract from args
 	return std::string{"Testfile"};
 }
 
--- tests/src/netlib/tests_unix.cpp
+++ tests/src/netlib/tests_unix.cpp
@@ -6,14 +6,29 @@
 
 #include <filesystem>
 #include <iostream>
+#include <stdexcept>
 
 TEST_CASE( "unix_domain_socket_simple_member_check1", "[.][net][unix_domain_socket]" )
 {
 
-	mart::nw::un::Socket sock1;
-	sock1.bind( mba::im_zstr( "sock1" ) );
-	mart::nw::un::Socket sock2(  //
-		mba::im_zstr( "sock2" ), //
-		mba::im_zstr( "sock1" )  //
+	mart::nw::un::Socket  sock1;
+	std::filesystem::path sock_path1( "sock1" );
+	std::filesystem::path sock_path2( "sock2" );
+
+	try {
+		std::filesystem::remove( sock_path1 );
+	} catch( const std::exception& e ) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::filesystem::remove( sock_path2 );
+	} catch( const std::exception& e ) {
+		std::cout << e.what() << std::endl;
+	}
+
+	sock1.bind( mba::im_zstr( sock_path1.string() ) );
+	mart::nw::un::Socket sock2(              //
+		mba::im_zstr( sock_path2.string() ), //
+		mba::im_zstr( sock_path1.string() )  //
 	);
 }
