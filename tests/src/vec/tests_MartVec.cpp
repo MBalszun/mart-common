dac21581c8cc22c80afbdf421bb2e5d669905431
MikeGitb <MikeGitb@users.noreply.github.com>
2022-03-24 13:57:01 +0100

    [Tests] Fix formatting in Vec tests

--- tests/src/vec/tests_MartVec.cpp
+++ tests/src/vec/tests_MartVec.cpp
@@ -19,12 +19,12 @@ template<class T, int N>
 
 struct Wrapper {
 	int                                       v;
-	[[maybe_unused]] friend constexpr Wrapper operator*( Wrapper l, int r ) { return {l.v * r}; }
-	[[maybe_unused]] friend constexpr Wrapper operator*( int l, Wrapper r ) { return {l * r.v}; }
-	[[maybe_unused]] friend constexpr Wrapper operator*( Wrapper l, Wrapper r ) { return {l.v * r.v}; }
-	[[maybe_unused]] friend constexpr Wrapper operator/( Wrapper l, int r ) { return {l.v / r}; }
-	[[maybe_unused]] friend constexpr Wrapper operator/( int l, Wrapper r ) { return {l / r.v}; }
-	[[maybe_unused]] friend constexpr Wrapper operator/( Wrapper l, Wrapper r ) { return {l.v / r.v}; }
+	[[maybe_unused]] friend constexpr Wrapper operator*( Wrapper l, int r ) { return { l.v * r }; }
+	[[maybe_unused]] friend constexpr Wrapper operator*( int l, Wrapper r ) { return { l * r.v }; }
+	[[maybe_unused]] friend constexpr Wrapper operator*( Wrapper l, Wrapper r ) { return { l.v * r.v }; }
+	[[maybe_unused]] friend constexpr Wrapper operator/( Wrapper l, int r ) { return { l.v / r }; }
+	[[maybe_unused]] friend constexpr Wrapper operator/( int l, Wrapper r ) { return { l / r.v }; }
+	[[maybe_unused]] friend constexpr Wrapper operator/( Wrapper l, Wrapper r ) { return { l.v / r.v }; }
 	friend constexpr bool                     operator==( Wrapper l, Wrapper r ) { return l.v == r.v; }
 };
 
@@ -43,10 +43,10 @@ constexpr mart::Vec<T, N> generate1()
 {
 	mart::Vec<T, N> vec{};
 
-	T v{-N / 2};
+	T v{ -N / 2 };
 	for( int i = 0; i < N; ++i ) {
 		vec[i] = v;
-		v      = v + T{1};
+		v      = v + T{ 1 };
 	}
 	return vec;
 }
@@ -55,10 +55,10 @@ template<class T, int N>
 constexpr mart::Vec<T, N> generate2()
 {
 	mart::Vec<T, N> vec{};
-	T               v{0};
+	T               v{ 0 };
 	for( int i = 0; i < N; ++i ) {
 		vec[i] = v;
-		v      = v + T{1};
+		v      = v + T{ 1 };
 	}
 	return vec;
 }
@@ -67,7 +67,7 @@ template<class T, int N>
 constexpr mart::Vec<T, N> generate3()
 {
 	mart::Vec<T, N> vec{};
-	T               v{1};
+	T               v{ 1 };
 	for( int i = 0; i < N; ++i ) {
 		vec[i] = v;
 		v      = v * 2;
@@ -139,48 +139,48 @@ static_assert( check_multiplication_division<int, 7>() );
 namespace element_wise_comparison_ops {
 
 using Vec_t = mart::Vec<int, 5>;
-constexpr Vec_t vec1{1, 2, 3, 4, 5};
-constexpr Vec_t vec2{2, 2, 2, 2, 2};
+constexpr Vec_t vec1{ 1, 2, 3, 4, 5 };
+constexpr Vec_t vec2{ 2, 2, 2, 2, 2 };
 
 using BVec_t = mart::Vec<bool, 5>;
 
-static_assert( mart::elementLess( vec1, vec2 ) == BVec_t{true, false, false, false, false} );
-static_assert( mart::elementLessEqual( vec1, vec2 ) == BVec_t{true, true, false, false, false} );
-static_assert( mart::elementGreater( vec1, vec2 ) == BVec_t{false, false, true, true, true} );
-static_assert( mart::elementGreaterEqual( vec1, vec2 ) == BVec_t{false, true, true, true, true} );
-static_assert( mart::elementNE( vec1, vec2 ) == BVec_t{true, false, true, true, true} );
-static_assert( mart::elementEquals( vec1, vec2 ) == BVec_t{false, true, false, false, false} );
+static_assert( mart::elementLess( vec1, vec2 ) == BVec_t{ true, false, false, false, false } );
+static_assert( mart::elementLessEqual( vec1, vec2 ) == BVec_t{ true, true, false, false, false } );
+static_assert( mart::elementGreater( vec1, vec2 ) == BVec_t{ false, false, true, true, true } );
+static_assert( mart::elementGreaterEqual( vec1, vec2 ) == BVec_t{ false, true, true, true, true } );
+static_assert( mart::elementNE( vec1, vec2 ) == BVec_t{ true, false, true, true, true } );
+static_assert( mart::elementEquals( vec1, vec2 ) == BVec_t{ false, true, false, false, false } );
 
 } // namespace element_wise_comparison_ops
 
 namespace element_Wise_logic_ops {
 
 using BVec_t = mart::Vec<bool, 5>;
-constexpr BVec_t vec1{true, false, true, false, true};
-constexpr BVec_t vec2{true, true, false, false, false};
+constexpr BVec_t vec1{ true, false, true, false, true };
+constexpr BVec_t vec2{ true, true, false, false, false };
 
-static_assert( mart::elementAnd( vec1, vec2 ) == BVec_t{true, false, false, false, false} );
-static_assert( mart::elementOr( vec1, vec2 ) == BVec_t{true, true, true, false, true} );
+static_assert( mart::elementAnd( vec1, vec2 ) == BVec_t{ true, false, false, false, false } );
+static_assert( mart::elementOr( vec1, vec2 ) == BVec_t{ true, true, true, false, true } );
 
 } // namespace element_Wise_logic_ops
 
 namespace element_wise_min_max {
 
 using Vec_t = mart::Vec<int, 5>;
-constexpr Vec_t vec1{1, 4, -2, 3, -10};
-constexpr Vec_t vec2{2, -4, -2, 3, 5};
+constexpr Vec_t vec1{ 1, 4, -2, 3, -10 };
+constexpr Vec_t vec2{ 2, -4, -2, 3, 5 };
 
-static_assert( mart::max( vec1, vec2 ) == Vec_t{2, 4, -2, 3, 5} );
-static_assert( mart::min( vec1, vec2 ) == Vec_t{1, -4, -2, 3, -10} );
+static_assert( mart::max( vec1, vec2 ) == Vec_t{ 2, 4, -2, 3, 5 } );
+static_assert( mart::min( vec1, vec2 ) == Vec_t{ 1, -4, -2, 3, -10 } );
 
 } // namespace element_wise_min_max
 
 namespace equal_unequal {
 using Vec_t = mart::Vec<int, 5>;
 
-constexpr Vec_t v1{1, 2, 3, 4, -5};
-constexpr Vec_t v2{1, 2, 3, 4, -5};
-constexpr Vec_t v3{1, 2, 3, 4, 5};
+constexpr Vec_t v1{ 1, 2, 3, 4, -5 };
+constexpr Vec_t v2{ 1, 2, 3, 4, -5 };
+constexpr Vec_t v3{ 1, 2, 3, 4, 5 };
 
 static_assert( v1 == v2 );
 static_assert( v1 != v3 );
@@ -194,50 +194,50 @@ static_assert( v3 != v2 );
 
 namespace random_math {
 
-constexpr mart::Vec3D<Wrapper> base{{1}, {1}, {2}};
+constexpr mart::Vec3D<Wrapper> base{ { 1 }, { 1 }, { 2 } };
 
-constexpr auto r1 = base * mart::Vec3D<int>{1, 2, -1};
+constexpr auto r1 = base * mart::Vec3D<int>{ 1, 2, -1 };
 static_assert( std::is_same_v<std::remove_cv_t<decltype( r1 )>, mart::Vec3D<decltype( Wrapper{} * int{} )>> );
-static_assert( r1 == mart::Vec3D<Wrapper>{{1}, {2}, {-2}} );
+static_assert( r1 == mart::Vec3D<Wrapper>{ { 1 }, { 2 }, { -2 } } );
 
 constexpr auto r2 = base * base;
 static_assert( std::is_same_v<mart::Vec3D<decltype( Wrapper{} * Wrapper{} )>, std::remove_cv_t<decltype( r2 )>> );
-static_assert( r2 == mart::Vec3D<Wrapper>{{1}, {1}, {4}} );
+static_assert( r2 == mart::Vec3D<Wrapper>{ { 1 }, { 1 }, { 4 } } );
 
-static_assert( 2 * base * 2 == mart::Vec3D<Wrapper>{{4}, {4}, {8}} );
+static_assert( 2 * base * 2 == mart::Vec3D<Wrapper>{ { 4 }, { 4 }, { 8 } } );
 } // namespace random_math
 
 namespace matrix_multiplication {
-constexpr auto mx1 = mart::Matrix<int, 3>{           //
-										  {0, 1, 2}, //
-										  {3, 4, 5}, //
-										  {6, 7, 8}};
+constexpr auto mx1 = mart::Matrix<int, 3>{              //
+										   { 0, 1, 2 }, //
+										   { 3, 4, 5 }, //
+										   { 6, 7, 8 } };
 
-constexpr auto mx2 = mart::Matrix<int, 3>{              //
-										  {12, 23, 34}, //
-										  {45, 56, 67}, //
-										  {78, 89, 90}};
+constexpr auto mx2 = mart::Matrix<int, 3>{                 //
+										   { 12, 23, 34 }, //
+										   { 45, 56, 67 }, //
+										   { 78, 89, 90 } };
 
-constexpr auto res = mart::Matrix<int, 3>{                 //
-										  {201, 234, 247}, //
-										  {606, 738, 820}, //
-										  {1011, 1242, 1393}};
+constexpr auto res = mart::Matrix<int, 3>{                    //
+										   { 201, 234, 247 }, //
+										   { 606, 738, 820 }, //
+										   { 1011, 1242, 1393 } };
 
 static_assert( res == mx_multiply( mx1, mx2 ) );
 
-static_assert( mart::Matrix<bool, 3>{{true, true, true}, //
-									 {true, true, true}, //
-									 {true, true, true}} //
+static_assert( mart::Matrix<bool, 3>{ { true, true, true },  //
+									  { true, true, true },  //
+									  { true, true, true } } //
 			   == mart::elementEquals( mx1, mx1 ) );
 
-static_assert( mart::Matrix<bool, 3>{{true, true, true},  //
-									 {true, false, true}, //
-									 {true, true, true}}  //
+static_assert( mart::Matrix<bool, 3>{ { true, true, true },  //
+									  { true, false, true }, //
+									  { true, true, true } } //
 			   != mart::elementEquals( mx1, mx1 ) );
 
-static_assert( mart::Matrix<bool, 3>{{false, false, false}, //
-									 {false, false, false}, //
-									 {false, false, false}} //
+static_assert( mart::Matrix<bool, 3>{ { false, false, false },  //
+									  { false, false, false },  //
+									  { false, false, false } } //
 			   == mart::elementNE( mx1, mx1 ) );
 
 } // namespace matrix_multiplication
@@ -247,8 +247,8 @@ constexpr bool check_constexpr_operator_availability()
 {
 	static_assert( std::is_trivial_v<mart::Vec<T, N>> || !std::is_trivial_v<T>, "mart::Vec is not a trivial type" );
 
-	constexpr auto                  v1 = generate1<T, N>();
-	[[maybe_unused]] constexpr auto t1 = v1.squareNorm();
+	constexpr auto                  v1  = generate1<T, N>();
+	[[maybe_unused]] constexpr auto t1  = v1.squareNorm();
 	[[maybe_unused]] constexpr auto t2  = v1.template toKDim<1>();
 	[[maybe_unused]] constexpr auto t3  = v1.template toKDim<2>();
 	[[maybe_unused]] constexpr auto t4  = v1.template toKDim<3>();
@@ -257,10 +257,10 @@ constexpr bool check_constexpr_operator_availability()
 	[[maybe_unused]] constexpr auto t7  = v1.template toKDim<6>();
 	[[maybe_unused]] constexpr auto t8  = v1.template toKDim<7>();
 	[[maybe_unused]] constexpr auto t9  = v1.template toKDim<8>();
-	[[maybe_unused]] constexpr auto t10  = v1.template toKDim<9>();
-	[[maybe_unused]] constexpr auto t11  = v1.template toKDim<10>();
-	[[maybe_unused]] constexpr auto t12  = v1.template toKDim<11>();
-	[[maybe_unused]] constexpr auto t13  = v1.template toKDim<12>();
+	[[maybe_unused]] constexpr auto t10 = v1.template toKDim<9>();
+	[[maybe_unused]] constexpr auto t11 = v1.template toKDim<10>();
+	[[maybe_unused]] constexpr auto t12 = v1.template toKDim<11>();
+	[[maybe_unused]] constexpr auto t13 = v1.template toKDim<12>();
 	return true;
 }
 
@@ -391,14 +391,14 @@ TEST_CASE( "MartVec_decimal_to_integral", "[vec]" )
 {
 	using DVec_t = mart::Vec<double, 5>;
 	using IVec_t = mart::Vec<int, 5>;
-	DVec_t vec1{2.3, 2.7, 0.0, -2.3, -2.7};
+	DVec_t vec1{ 2.3, 2.7, 0.0, -2.3, -2.7 };
 	// BVec_t vec2{ true, true, false, false, false };
 
-	CHECK( mart::ceil( vec1 ) == DVec_t{3, 3, 0, -2, -2} );
-	CHECK( mart::floor( vec1 ) == DVec_t{2, 2, 0, -3, -3} );
-	CHECK( mart::round( vec1 ) == DVec_t{2, 3, 0, -2, -3} );
-	CHECK( mart::iround( vec1 ) == IVec_t{2, 3, 0, -2, -3} );
-	CHECK( mart::lround( vec1 ) == mart::Vec<long, 5>{2, 3, 0, -2, -3} );
+	CHECK( mart::ceil( vec1 ) == DVec_t{ 3, 3, 0, -2, -2 } );
+	CHECK( mart::floor( vec1 ) == DVec_t{ 2, 2, 0, -3, -3 } );
+	CHECK( mart::round( vec1 ) == DVec_t{ 2, 3, 0, -2, -3 } );
+	CHECK( mart::iround( vec1 ) == IVec_t{ 2, 3, 0, -2, -3 } );
+	CHECK( mart::lround( vec1 ) == mart::Vec<long, 5>{ 2, 3, 0, -2, -3 } );
 
 	// CHECK(mart::lround(vec1) == IVec_t{ 2, 3, 0, -2, -3 });
 }
